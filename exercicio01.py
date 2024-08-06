88e31f181648dd98717564af9f1d7e37129a97e4
pamelafrr <pamela.ferreira@al.infnet.edu.br>
2024-08-06 03:02:44 +0000

    concluido a atividade 1

--- exercicio01.py
+++ exercicio01.py
@@ -13,22 +13,28 @@ print("Hello World!")
 # Qual parte do código de exemplo é a instrução de saída?
 
 # Responda:
+# A instrução de saída é o print.
 
 # Qual parte do código de exemplo é a string?
 
 # Responda:
+# A string é o texto entre as aspas, ou seja, é o Hello World.
 
 # Qual seria a saída do código print("I love Computing")?
 
 # Responda:
+# A saída seria o texto do print "I love Computing".
 
 # O que aconteceria se o código print("I love Comping") fosse executado?
 
 # Responda:
+# Ele aparecerá na tela a mensagem "I love Comping"
 
 # O que aconteceria se o código print("I love Computing" fosse executado?
 
 # Responda:
+# Ele irá gerar um erro, porque nao foi fechado o metódo print, ou seja, o correto seria: print("I love Computing")
+
 """Ajuda! Meu código não funciona!
 
 Certifique-se de verificar os seguintes itens:
@@ -36,6 +42,3 @@ Certifique-se de verificar os seguintes itens:
 O print não tem letra maiúscula;
 O texto da string é cercado por aspas duplas (“) ou aspas simples (‘);
 A string (incluindo aspas) é cercada por parênteses."""
-
-
-print(" testando um novo commit ")
