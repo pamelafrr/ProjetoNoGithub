725392ec71f59593ab2bf57b05605f1750c7c056
pamelafrr <pamela.ferreira@al.infnet.edu.br>
2024-08-12 02:38:44 +0000

    exercicio 11 feito

--- exercicio11.py
+++ exercicio11.py
@@ -4,4 +4,10 @@
 # Faça um programa que somará dois números e mostrará a média desses números como resultado.
 
 # Escreva seu código aqui
+numero1 = input("digite o primeiro numero")
+numero2 = input("digite o segundo numero")
 
+resultado = int(numero1) + int(numero2)  
+resultado = resultado / 2 
+
+print (resultado)
